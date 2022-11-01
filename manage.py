77d70385db7c4c827cd93a0a94767e7f716442c8
JeniferSanchez <113454947+JeniferSanchez@users.noreply.github.com>
2022-10-31 23:59:58 -0500

    Add files via upload

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sioop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghorarios.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
